de2bd5d88bef8e67bc1e6482d39b3dacf2b86994
Ponomarev Sergei <papasergiusv@gmail.com>
2019-07-25 12:22:11 +0300

    Add eslinter & rubocop.yml

--- app/javascript/components/pages/Welcome.jsx
+++ app/javascript/components/pages/Welcome.jsx
@@ -1,5 +1,5 @@
-import React from "react"
-import PropTypes from "prop-types"
+import React from "react";
+
 class Welcome extends React.Component {
   render () {
     return (
@@ -10,4 +10,4 @@ class Welcome extends React.Component {
   }
 }
 
-export default Welcome
+export default Welcome;
