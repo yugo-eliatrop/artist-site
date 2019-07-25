3410f0ad608692291f571c8345ad8bfa1a5325f3
Ponomarev Sergei <papasergiusv@gmail.com>
2019-07-25 18:53:46 +0300

    Connect bootstrap

--- app/javascript/components/pages/Welcome.jsx
+++ app/javascript/components/pages/Welcome.jsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { Component, Fragment } from "react";
 
-class Welcome extends React.Component {
+class Welcome extends Component {
   render () {
     return (
-      <React.Fragment>
-        HW
-      </React.Fragment>
+      <Fragment>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <p>H</p>
+            </div>
+            <div className="col-md-6">
+              <p>W</p>
+            </div>
+          </div>
+        </div>
+      </Fragment>
     );
   }
 }
