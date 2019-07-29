404f82aceef772b980aeb4b19dcea9f9897c162b
Ponomarev Sergei <papasergiusv@gmail.com>
2019-07-29 18:57:48 +0300

    Resolve class problem

--- app/javascript/components/pages/Welcome.jsx
+++ app/javascript/components/pages/Welcome.jsx
@@ -3,12 +3,12 @@ import React, { useState } from "react";
 import FirstScreen from "../shared/firstScreen/FirstScreen";
 import MainMenu from "../shared/menu/MainMenu";
 
-const Welcome = () => {
+const Welcome = props => {
   const [menuIsVisible, toggleMenu] = useState(false);
   return (
     <div>
       <FirstScreen openMenu={() => toggleMenu(true)} />
-      {menuIsVisible && <MainMenu close={() => toggleMenu(false)} />}
+      {menuIsVisible && <MainMenu {...props} close={() => toggleMenu(false)} />}
     </div>
   );
 };
--- app/javascript/components/shared/menu/MainMenu.jsx
+++ app/javascript/components/shared/menu/MainMenu.jsx
@@ -1,50 +1,66 @@
-import React from "react";
+import React, { Component } from "react";
 import CSSModules from "react-css-modules";
 import { FaInstagram, FaVk, FaTimes } from "react-icons/fa";
 
-// import { showWindow, hideWindow } from "../../../libs/animation";
+import { showWindow, hideWindow } from "../../../libs/animation";
 import styles from "./MainMenu.module.scss";
 
-const MainMenu = props => {
-  return (
-    <div styleName="wrapper">
-      <div className="container">
-        <div className="row">
-          <div className="col-12" styleName="row">
-            <div styleName="gold">LOGO</div>
-            <div styleName="close-button"><FaTimes onClick={props.close} /></div>
-          </div>
-          <div className="col-12" styleName="row">
-            <div>
-              <ul>
-                <li>Main</li>
-                <li>Album</li>
-                <li>Point3</li>
-                <li>Point4</li>
-              </ul>
+class MainMenu extends Component {
+  constructor(props) {
+    super(props);
+    this.wrapper = React.createRef();
+  }
+
+  componentDidMount = () => showWindow(this.wrapper.current, 150, "flex");
+
+  close = () => {
+    hideWindow(this.wrapper.current);
+    setTimeout(this.props.close, 150);
+  }
+
+  render () {
+    return (
+      <div styleName="wrapper" ref={this.wrapper}>
+        <div className="container">
+          <div className="row">
+            <div className="col-12" styleName="row">
+              <div styleName="gold">LOGO</div>
+              <div styleName="close-button">
+                <FaTimes onClick={this.close} />
+              </div>
             </div>
-            <div>
-              <p>Something</p>
-              <p>Something</p>
-              <p>Something</p>
+            <div className="col-12" styleName="row">
+              <div>
+                <ul>
+                  <li>Main</li>
+                  <li>Albums</li>
+                  <li>About</li>
+                </ul>
+              </div>
+              <div>
+                <p>Something</p>
+                <p>Something</p>
+                <p>Something</p>
+                <p>Something</p>
+              </div>
             </div>
-          </div>
-          <div className="col-12" styleName="row">
-            <hr />
-          </div>
-          <div className="col-12" styleName="row">
-            <div>
-              <p>Address</p>
+            <div className="col-12" styleName="row">
+              <hr/>
             </div>
-            <div>
-              <FaInstagram />
-              <FaVk />
+            <div className="col-12" styleName="row">
+              <div>
+                <p>Address</p>
+              </div>
+              <div>
+                <FaInstagram />
+                <FaVk />
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default CSSModules(MainMenu, styles, { allowMultiple: true });
