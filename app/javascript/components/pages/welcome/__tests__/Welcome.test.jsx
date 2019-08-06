196567b443eea44405aa60241013a633745b1998
Ponomarev Sergei <papasergiusv@gmail.com>
2019-08-06 16:34:38 +0300

    Add First screen tests

--- app/javascript/components/pages/welcome/__tests__/Welcome.test.jsx
+++ app/javascript/components/pages/welcome/__tests__/Welcome.test.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 // import { create, update } from "react-test-renderer";
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
 import { shallowToJson } from "enzyme-to-json";
 import "../../../../setupTests";
 
 import Welcome from "../Welcome";
 
-describe('Welcome', () => {
-  it('should render FirstScreen component only', () => {
-    const output = shallow(<Welcome/>);
-    expect(shallowToJson(output)).toMatchSnapshot();
+describe("Welcome component", () => {
+  it("should render FirstScreen component only", () => {
+    const wrapper = shallow(<Welcome/>);
+    expect(shallowToJson(wrapper)).toMatchSnapshot();
+    expect(wrapper.children().length).toEqual(1);
+  });
+
+  it("should show menu after first screen menu button click", () => {
+    const wrapper = mount(<Welcome />);
+    const openBtn = wrapper.find("svg");
+    openBtn.simulate("click");
+    expect(wrapper.find("div").first().children().length).toEqual(2);
   });
 });
