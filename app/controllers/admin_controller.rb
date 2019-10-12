4e4792f5ced71471e3e73ecff1f81041f5dafa27
Ponomarev Sergei <papasergiusv@gmail.com>
2019-10-12 20:42:07 +0300

    Add simple admin service

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,59 @@
+class AdminController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    render component: 'pages/admin/Main', props: {
+      albums: Album.all.order(:priority),
+      texts: Text.all.order(:id),
+      contacts: Contact.all.order(:id),
+      csrf_token: form_authenticity_token
+    }
+  end
+
+  def update
+    case params[:type]
+    when 'text'
+      update_text
+    when 'contact'
+      update_contact
+    else
+      head 404
+    end
+  end
+
+  private
+
+  def update_text
+    text = Text.find(params[:id])
+    if text.update text_params
+      head :ok
+    else
+      render json: {
+        type: 'text',
+        id: text.id,
+        errors: text.errors
+      }, status: :unprocessable_entity
+    end
+  end
+
+  def update_contact
+    contact = Contact.find(params[:id])
+    if contact.update contact_params
+      head :ok
+    else
+      render json: {
+        type: 'contact',
+        id: contact.id,
+        errors: contact.errors
+      }, status: :unprocessable_entity
+    end
+  end
+
+  def text_params
+    params.permit(:title, :content)
+  end
+
+  def contact_params
+    params.permit(:login, :address)
+  end
+end
