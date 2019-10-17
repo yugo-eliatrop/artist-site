d65934e7b0e759cba8c6d1efa08290377f07b2dc
Ponomarev Sergei <papasergiusv@gmail.com>
2019-10-17 18:40:55 +0300

    Do Main admin component refactoring

--- app/javascript/components/pages/admin/Main.jsx
+++ app/javascript/components/pages/admin/Main.jsx
@@ -2,6 +2,8 @@ import React from "react";
 // import PropTypes from "prop-types";
 import CSSModules from "react-css-modules";
 
+import TextSettings from "./text/Text";
+import ContactsSettings from "./contacts/Contacts";
 import Routes from "../../../libs/routes";
 import styles from "./Main.module.scss";
 
@@ -26,55 +28,8 @@ const Main = props => {
             <h2>Admin settings</h2>
           </div>
         </div>
-        <div styleName="texts point" className="row">
-          <div className="col-12">
-            <h3 styleName="point-title">Text settings</h3>
-          </div>
-          {
-            texts.map(text =>
-              <div key={text.id} className="col-12 col-md-6">
-                <form onSubmit={handleSubmit}>
-                  <h4>{text.key}</h4>
-                  <p>Title:</p>
-                  <input name="title" type="text" defaultValue={text.title} />
-                  <p>Text:</p>
-                  <textarea name="content" defaultValue={text.content} />
-                  <input name="key" hidden type="text" readOnly value={text.key} />
-                  <input name="type" hidden type="text" readOnly value="text" />
-                  <input name="id" hidden type="number" readOnly value={text.id} />
-                  <button type="submit">Update</button>
-                </form>
-              </div>
-            )
-          }
-        </div>
-        <div styleName="contacts point" className="row">
-          <div className="col-12">
-            <h3 styleName="point-title">Contact settings</h3>
-          </div>
-          <div className="col-12">
-            <div styleName="contact-field">
-              <span>Service</span>
-              <span>Login</span>
-              <span>Address</span>
-            </div>
-          </div>
-          {
-            contacts.map(contact =>
-              <div key={contact.id} className="col-12">
-                <form styleName="contact-field" onSubmit={handleSubmit}>
-                  <span>{contact.service}</span>
-                  <input name="login" type="text" defaultValue={contact.login || ""} />
-                  <input name="address" type="text" defaultValue={contact.address} />
-                  <input name="service" hidden type="text" readOnly value={contact.service} />
-                  <input name="type" hidden type="text" readOnly value="contact" />
-                  <input name="id" hidden type="number" readOnly value={contact.id} />
-                  <button type="submit">Update</button>
-                </form>
-              </div>
-            )
-          }
-        </div>
+        <TextSettings texts={texts} handleSubmit={handleSubmit} />
+        <ContactsSettings contacts={contacts} handleSubmit={handleSubmit} />
       </div>
     </div>
   );
