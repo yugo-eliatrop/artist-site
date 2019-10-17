26438a32e64d72b3c00c38f02403ca110a452341
Ponomarev Sergei <papasergiusv@gmail.com>
2019-10-17 20:11:22 +0300

    Add convertAlbums callback

--- app/javascript/components/pages/admin/album/List.jsx
+++ app/javascript/components/pages/admin/album/List.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 // import PropTypes from "prop-types";
 import CSSModules from "react-css-modules";
 import { FaArrowAltCircleUp, FaArrowAltCircleDown } from "react-icons/fa";
@@ -7,12 +7,28 @@ import { FaArrowAltCircleUp, FaArrowAltCircleDown } from "react-icons/fa";
 import styles from "./List.module.scss";
 
 const List = props => {
-  const { albums } = props;
+  const convertAlbums = useCallback(() => {
+    let obj = {};
+    props.albums.forEach(album => { obj[album.id] = album; });
+    obj["count"] = props.albums.length;
+    return obj;
+  }, [props.albums]);
+
+  const albums = convertAlbums();
 
   const [priorityIsBeingEdited, setPriorityChangingStatus] = useState(false);
+  const [priority, setPriority] = useState(props.albums.map(album => album.id));
 
   const changePriority = () => {
     setPriorityChangingStatus(false);
+  };
+
+  const up = id => {
+
+  };
+
+  const down = id => {
+
   };
 
   return (
@@ -32,21 +48,23 @@ const List = props => {
         }
       </div>
       {
-        albums.map((album, i) =>
-          <div key={album.name} className="col-12" styleName="album">
-            <div styleName="left-side">
-              {
-                priorityIsBeingEdited &&
-                <div styleName="arrows">
-                  <button disabled={i === 0}>
-                    <FaArrowAltCircleUp />
-                  </button>
-                  <button disabled={i === albums.length - 1}>
-                    <FaArrowAltCircleDown />
-                  </button>
-                </div>
-              }
-              <strong>{album.name}</strong>
+        priority.map((id, i) =>
+          <div key={albums[id].name} className="col-12" styleName="album">
+            <div styleName="panel">
+              <div styleName="left-side">
+                {
+                  priorityIsBeingEdited &&
+                  <div styleName="arrows">
+                    <button onClick={() => up(id)} disabled={i === 0}>
+                      <FaArrowAltCircleUp />
+                    </button>
+                    <button onClick={() => down(id)} disabled={i === albums.count - 1}>
+                      <FaArrowAltCircleDown />
+                    </button>
+                  </div>
+                }
+                <strong>{albums[id].name}</strong>
+              </div>
             </div>
           </div>
         )
